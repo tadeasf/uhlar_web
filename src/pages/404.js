b25f5f30327c7501deb6a61196a8bcec6c14e406
tadeasf <taddy.fort@gmail.com>
2023-09-16 06:20:55 +0000

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,10 @@
-import * as React from "react"
-import Layout from "../components/Layout"
-import { Link } from "gatsby"
-import Seo from "../components/SEO"
-import Button from "../components/Button/Button"
+/** @format */
+
+import * as React from "react";
+import Layout from "../components/Layout";
+import { Link } from "gatsby";
+import Seo from "../components/SEO";
+import Button from "../components/Button/Button";
 
 const error = () => {
   return (
@@ -11,18 +13,15 @@ const error = () => {
       <Layout>
         <div className="section">
           <div className="container container__tight">
-            <h1>Sorry.</h1>
-            <h3>This page has moved or no longer exists.</h3>
-            <p>
-              Please choose another option from the menu above, or to return
-              home, press the button below.
-            </p>
-            <Button text="Return Home" to="/" as={Link} />
+            <h1>Omlouvám se.</h1>
+            <h3>Tato stránka neexistuje.</h3>
+            <p>Prosím, využij menu nebo tlačítko Domů.</p>
+            <Button text="E.T. Volá Domů!" to="/" as={Link} />
           </div>
         </div>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default error
+export default error;
