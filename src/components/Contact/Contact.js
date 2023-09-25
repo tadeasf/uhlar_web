ed182ea90052af728c6033a7048cd67d618b53a6
tadeasf <taddy.fort@gmail.com>
2023-09-25 20:26:11 +0000

    first?

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
     <ContactStyles className="section">
       <form name="contact" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
-        <input placeholder="Vaše jméno..." type="text" name="name" />
+        <input placeholder="Vaše jméno..." type="text" name="first_name" />
         <input placeholder="Váš e-mail..." type="email" name="email" />
         <textarea
           placeholder="Vaše zpráva..."
